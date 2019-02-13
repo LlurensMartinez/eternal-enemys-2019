52a0e6ab9488da64d60a243eaddf97545ac3d139
Llorenç <crack.one.7@gmail.com>
2019-02-13 14:05:35 +0100

    Splash

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 'use strict'
+
+const main = () => {
+
+    const buildDom = (html) => {
+        const main = document.querySelector('main');
+        main.innerHTML = html;
+        return main;
+
+    };
+
+
+    const buildSplashScreen = () => {
+        const splashScreen = buildDom(`
+        <section class="splash-screen"></section>
+          <h1>Eternal Enemies </h1>
+          <button>Start</button>
+        <h1>Hello<h1>
+        `)
+    };
+
+    buildSplashScreen();
+
+};
+
+
+
+window.addEventListener('load', main);
